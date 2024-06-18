3e83b1f41a5ce636b17706a7818d97ecb5a3b97f
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-06-18 13:17:43 +0900

    リストに数字を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Main {
 
   public static void main(String[] args) {
-    List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12);
     System.out.println(numberList.stream()
         .filter(n -> n % 2 == 1)
         .collect(Collectors.averagingInt(n -> n)));
